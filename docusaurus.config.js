58c8c652843f28e825b8bc2375e2d141a760fae1
Mauriceac <maurice.ac@gmail.com>
2021-04-08 20:23:25 -0400

    Updated repo location. Retries PR49

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -116,7 +116,7 @@ module.exports = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           editUrl:
-            'https://github.com/Cotalker/cotalker-doc/tree/master/',
+            'https://github.com/Cotalker/documentation/tree/master/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
