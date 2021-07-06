e7453aa655a6f2e590260566abb99dcd3397648d
Maurice Alvarado <maurice.ac@gmail.com>
2021-07-06 16:56:27 -0400

    blog

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -124,7 +124,7 @@ module.exports = {
         },
         blog: {
           blogSidebarCount: 'ALL',
-          blogSidebarTitle: 'Latest features:',
+          blogSidebarTitle: `What's New:`,
           feedOptions: {
             type: 'all',
           },
