193f91d82b8da20d859989342a174516890055c7
martinmiranda14 <martin.miranda.14@sansano.usm.cl>
2022-07-08 16:49:48 -0400

    -fix: first test

---
+++ tools/script.mjs
@@ -0,0 +1,27 @@
+#!/usr/bin/env zx
+
+/* Set context */
+/* Create i18n-es files  */
+
+// const path = require('path');
+// const i18nFolder = path.resolve(__dirname,'../i18n/es/docusaurus-plugin-content-docs/current');
+// const baseFolder = path.resolve(__dirname,'../docs');
+// await $`cd ${baseFolder}`
+// const folders = await $`ls * -la ${baseFolder} `
+
+//folders.forEach(folder => { 
+//    console.log(folder)
+    
+//});
+
+return {
+    translateThemeConfig({themeConfig, translationFiles}) {
+        console.log(themeConfig)
+      },
+}
+
+console.log(folders)
+
+
+
+
