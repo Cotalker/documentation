57aac3d17f74543a0f737101178d41ffbd7fce0c
martinmiranda14 <martin.miranda.14@sansano.usm.cl>
2022-07-11 16:14:25 -0400

    -feature: create translate script

--- tools/script.mjs
+++ tools/script.mjs
@@ -2,13 +2,13 @@
 import { $, cd } from "zx";
 const fs = require('fs');
 
-
-/* Set context */
-/* Create i18n-es files  */
-
 const path = require('path');
 const i18nFolder = path.resolve(__dirname,'../i18n/es/docusaurus-plugin-content-docs/current');
 const baseFolder = path.resolve(__dirname,'../docs/');
+const baseBlogFolder = path.resolve(__dirname,'../blog/');
+const i18nBlogFolder = path.resolve(__dirname,'../i18n/es/docusaurus-plugin-content-blog/');
+
+var caution = "\n:::caution Advertencia\nEsta página aún no se encuentra traducida al español.\n:::";
 
 const getFiles = async (basePath) => {
     cd(basePath);
@@ -16,25 +16,36 @@ const getFiles = async (basePath) => {
     return files;
 }
 
-var data = "\nLearn Node.js with the help of well built Node.js Tutorial.\n";
+const insertCaution = async (data , file) => {
+    var formatedData = data.split("\n");
+    fs.openSync(file, "a");
+    let title = false;
+    for (let i = 0; i < formatedData.length; i++){
+        if ( i == 0 && !title  ) title = true;
+        fs.appendFileSync(file, formatedData[i]+'\n');
+        if( i > 1 && formatedData[i] == '---' && title ){
+            title = false;
+            fs.appendFileSync(file, caution+'\n');
+        }
+    }
+}
 
 const fileList = await getFiles(baseFolder);
 
-fileList.forEach(file => {
+fileList.forEach( async file => {
     const base = file.replace('./','');
     if(base === '.') return;
-    const baseLength = base.length
+    const baseLength = base.length;
     if(!base.substring(baseLength - 4).includes('.')) {
-        $`mkdir -p ${i18nFolder}/${base}`
+        await $`mkdir -p ${i18nFolder}/${base}`
         return;
     }
     else if (base.substring(baseLength - 3) == '.js' ) {
-        console.log('AA')
         if (fs.existsSync(`${i18nFolder}/${base}`)) {
                 return;
             }
         else {
-            $`cp ${baseFolder}/${base} ${i18nFolder}/${base}`;
+            await $`cp ${baseFolder}/${base} ${i18nFolder}/${base}`;
         }
           
     }
@@ -43,17 +54,29 @@ fileList.forEach(file => {
             return;
         }
         else {
-            $`cp ${baseFolder}/${base} ${i18nFolder}/${base}`;
-            fs.appendFile(`${i18nFolder}/${base}`,data, 'utf8',
-                // callback function
-                function(err) {     
-                    if (err) throw err;
-                    // if no error
-                    console.log("Data is appended to file successfully.")
-            });
+            const baseFile = `${baseFolder}/${base}`
+            const newFile = `${i18nFolder}/${base}`
+            fs.readFile( baseFile , "utf-8", (err, data) => {insertCaution(data,newFile)})
         }
     }
 });
+
+const blogList = await getFiles(baseBlogFolder);
+
+blogList.forEach( async file => {
+    const base = file.replace('./','');
+    if(base === '.') return;
+    if (fs.existsSync(`${i18nBlogFolder}/${base}`)) {
+        return;
+    }
+    else {
+        const baseFile = `${baseBlogFolder}/${base}`
+        const newFile = `${i18nBlogFolder}/${base}`
+        fs.readFile( baseFile , "utf-8", (err, data) => {insertCaution(data,newFile)});
+    }
+});
+
+
 
 
 
